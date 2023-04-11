5dac3ea714acde4166ba71d6a35c26acf02217c0
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-04-11 15:45:21 -0400

    Implement asynchronous requests

--- src/DataSource.php
+++ src/DataSource.php
@@ -2,9 +2,19 @@
 
 namespace HelioviewerEventInterface;
 
+use \DateTime;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Promise\PromiseInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * A datasource represents an external data source which returns JSON data that we would like to include in Helioviewer.
+ * This class defines methods to query data from an external datasource given the appropriate metadata.
+ */
 class DataSource {
     public string $name;
-    protected string $url;
+    protected string $uri;
     protected string $startName;
     protected string $endName;
     protected string $dateFormat;
@@ -19,12 +29,41 @@ class DataSource {
      * @param string $dateFormat The format to use for the dates.
      * @param string $translator The name of the translator class to use for this data source.
      */
-    public function __construct(string $name, string $url, string $startName, string $endName, string $dateFormat, string $translator) {
+    public function __construct(string $name, string $uri, string $startName, string $endName, string $dateFormat, string $translator) {
         $this->name = $name;
-        $this->url = $url;
+        $this->uri = $uri;
         $this->startName = $startName;
         $this->endName = $endName;
         $this->dateFormat = $dateFormat;
         $this->translator = $translator;
+    }
+
+    /**
+     * Queries the data source asynchronously for relevant data between the start and end times.
+     * @param DateTime $start Start of time range
+     * @param DateTime $end End of time range
+     * @return PromiseInterface
+     */
+    public function getAsync(DateTime $start, DateTime $end): PromiseInterface {
+        // Convert input dates to strings
+        $startString = $start->format($this->dateFormat);
+        $endString = $end->format($this->dateFormat);
+        // Perform HTTP request to the source url
+        $client = new Client(["base_uri" => $this->uri]);
+        // Define the request with the date range as query parameters
+        $promise = $client->requestAsync('GET', '', [
+            'query' => [$this->startName => $startString, $this->endName => $endString]
+        ]);
+        return $promise->then(
+            // Decode the json result on a successful request
+            function (ResponseInterface $response) {
+                return json_decode($response->getBody()->getContents());
+            },
+            // Fail gracefully on failure by logging the result and returning an empty list representing no data available from this source.
+            function (RequestException $e) {
+                error_log($e->getMessage());
+                return [];
+            }
+        );
     }
 }
---
+++ tests/DataSourceTest.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+use HelioviewerEventInterface\DataSource;
+use PHPUnit\Framework\TestCase;
+
+final class DataSourceTest extends TestCase
+{
+    private DateTime $START_DATE;
+    private DateTime $END_DATE;
+    public function __construct(string $name) {
+        parent::__construct($name);
+        $this->START_DATE = new DateTime('2023-04-01');
+        $this->END_DATE = new DateTime('2023-04-02');
+    }
+
+    public function testAsyncQuery(): void
+    {
+        // Test via the DONKI CME data source
+        $datasource = new DataSource("Donki", "https://kauai.ccmc.gsfc.nasa.gov/DONKI/WS/get/CME", "startDate", "endDate", "Y-m-d", DonkiCme::class);
+        $promise = $datasource->getAsync($this->START_DATE, $this->END_DATE);
+        $data = $promise->wait();
+        $this->assertEquals(8, count($data));
+    }
+}
+
