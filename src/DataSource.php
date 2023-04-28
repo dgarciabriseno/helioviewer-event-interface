212264c02cea407bcfddf91b2c3a5092be0a019c
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-04-28 14:22:54 -0400

    update datasource cache key to fix duped key

--- src/DataSource.php
+++ src/DataSource.php
@@ -46,7 +46,7 @@ class DataSource {
      */
     public function GetCacheKey(\DateTimeInterface $date, \DateInterval $interval): string {
         // This should be unique across all data sources
-        $data = "$this->source $this->name " . json_encode($this->queryParameters) . json_encode($this->extra);
+        $data = "$this->source $this->name $this->pin" . json_encode($this->queryParameters) . json_encode($this->extra);
         return Cache::CreateKey($data, $date, $interval, $this->dateFormat);
     }
 
