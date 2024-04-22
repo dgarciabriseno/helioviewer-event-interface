f556c1b10149019c2b197710535f4c566b715c36
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2024-04-22 14:58:55 -0400

    Add associate function to csv parser

--- src/Util/CsvParser.php
+++ src/Util/CsvParser.php
@@ -9,6 +9,15 @@ use ValueError;
  */
 class CsvParser {
     /**
+     * Associates the $values to the given $keys array
+     * @throws ValueError when the number of keys doesn't match the number of values in the line.
+     */
+    public static function Associate(array $keys, array $values): array {
+        // Array combine will through ValueError if lengths don't match
+        return array_combine($keys, $values);
+    }
+
+    /**
      * Converts a line of CSV text into a key->value pair array using the given
      * keys. The keys should align to each value in the comma separated list.
      *
@@ -17,11 +26,7 @@ class CsvParser {
     public static function ToArray(array $keys, string $line, string $separator = ','): array {
         $values = explode($separator, $line);
         self::ValidateSameLength($keys, $values);
-        $out = [];
-        foreach ($keys as $idx => $key) {
-            $out[$key] = $values[$idx];
-        }
-        return $out;
+        return self::Associate($keys, $values);
     }
 
     /** Throws a ValueError if the length of $a doesn't match the length of $b */
