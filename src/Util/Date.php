837b1bd4119984af40929060d646c2ee8b073267
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2024-04-22 14:47:12 -0400

    Add more date utility functions

--- src/Util/Date.php
+++ src/Util/Date.php
@@ -35,6 +35,24 @@ class Date {
         }
     }
 
+    /**
+     * Checks if two date ranges overlap
+     */
+    public static function RangeOverlap(DateTimeInterface $start1, DateTimeInterface $end1, DateTimeInterface $start2, DateTimeInterface $end2): bool {
+        return ($start1 <= $end2) && ($end1 >= $start2);
+    }
+
+    /**
+     * Checks if the given date is between the given start and end dates.
+     * @return True if $start <= $date <= $end
+     */
+    public static function Between(DateTimeInterface $date, DateTimeInterface $start, DateTimeInterface $end): bool {
+        if (($start < $date) && ($date < $end)) {
+            return true;
+        }
+        return false;
+    }
+
     private static function ParseDateString(?string $date): ?DateTime {
         try {
             return new DateTime($date);
