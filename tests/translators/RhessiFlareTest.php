2d516fc1cd2a364c187065199177399ecf974bed
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2024-05-16 09:49:58 -0400

    Fix rhessi short label test

--- tests/translators/RhessiFlareTest.php
+++ tests/translators/RhessiFlareTest.php
@@ -5,9 +5,9 @@ use HelioviewerEventInterface\Translator\RhessiFlare;
 
 final class RhessiFlareTest extends TestCase
 {
-    public function testShortLabelDefaultLabel(): void 
+    public function testShortLabelDefaultLabel(): void
     {
-        $data = explode(",", "2021229,2002-02-12 02:15:24,2002-02-12 02:19:22,2002-02-12 02:25:48,46,75270,958,-118,25-50,5,2,2002/02/12/20020212_0215_0225/hsi_20020212_0215_0225.html");
+        $data = explode(",", "2021229,2002-02-12 02:15:24,2002-02-12 02:19:22,2002-02-12 02:25:48,46,75270,958,-118,25-50,100,5,2,2002/02/12/20020212_0215_0225/hsi_20020212_0215_0225.html");
 
         $rhessi_flare = new RhessiFlare($data);
 
